c5a1358e09f49bf46c46fce3f4e0a488508b5c10
vattvichet <vichetvattedu@gmail.com>
2022-05-06 12:19:50 +0700

    Initial Commit

---
+++ StarQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarQuiz
+//
+//  Created by vichet vatt on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
